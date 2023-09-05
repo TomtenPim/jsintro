49ddc625c058c9b11a3bb8f73d51f02594111cc1
= <=>
2023-09-05 14:26:31 +0200

    Skapat variabler.js

---
+++ variabler.js
@@ -0,0 +1,12 @@
+let name = "Simon"
+console.log(typeof(name))
+const greeting = `Hej ${name}`
+console.log(greeting)
+let age = 16
+console.log(typeof(age))
+let x = Number.MAX_VALUE
+console.log(name + x)
+
+let y = x * 2 +1
+console.log(typeof(y))
+console.log(isNaN(y))
